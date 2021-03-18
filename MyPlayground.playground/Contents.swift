3f809e5c3331323024f51c3c9179d981620785ec
马超 <chao.ma2@ly.com>
2021-03-18 15:04:11 +0800

    feat: 提交

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
+func example(of desc: String, _ block: () -> Void) {
+    print("====== Example of \(desc) ======")
+    block()
+}
+
+
+example(of: "creating and linking nodes") {
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+    
+    node1.next = node2
+    node2.next = node3
+    
+    print(node1)
+}
