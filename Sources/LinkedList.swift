bc59eaa23c88bf2c74ae79f87c1a77bf83c07559
马超 <chao.ma2@ly.com>
2021-03-19 16:07:01 +0800

    feat： 实现了链表的代码

--- Sources/LinkedList.swift
+++ Sources/LinkedList.swift
@@ -10,6 +10,16 @@ public struct LinkedList<Value> {
         return head == nil
     }
     
+    public var nodes: [Node<Value>] {
+        var nodes = [Node<Value>]()
+        var currentNode = head
+        while currentNode != nil {
+            nodes.append(currentNode!)
+            currentNode = currentNode!.next
+        }
+        return nodes
+    }
+    
     public mutating func push(_ value: Value) {
         head = Node(value: value, next: head)
         if tail == nil {
@@ -49,6 +59,49 @@ public struct LinkedList<Value> {
         
         return node.next!
     }
+    
+    public mutating func pop() -> Value? {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    public mutating func removeLast() -> Value? {
+        guard let head = head else {
+            return nil
+        }
+        
+        guard head.next != nil else {
+            return pop()
+        }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        
+        return current.value
+    }
+    
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
 }
 
 extension LinkedList: CustomStringConvertible {
@@ -57,6 +110,48 @@ extension LinkedList: CustomStringConvertible {
             return "Empty list"
         }
         return String(describing: head)
+    }
+}
+
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public static func == (lhs: LinkedList<Value>.Index, rhs: LinkedList<Value>.Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        public static func < (lhs: LinkedList<Value>.Index, rhs: LinkedList<Value>.Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node, next: { $0?.next })
+            return nodes.contains { $0 === rhs.node }
+        }
+        
+        
+        public var node: Node<Value>?
+    }
+    
+    public var startIndex: Index {
+        return Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        return Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        return Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> Value {
+        return position.node!.value
     }
 }
 
---
+++ SwiftDataStructuresAndAlgorithms/LinkedListView.swift
@@ -0,0 +1,153 @@
+//
+//  LinkedListView:.swift
+//  SwiftDataStructuresAndAlgorithms
+//
+//  Created by MC on 2021/3/18.
+//
+
+import SwiftUI
+import Combine
+
+class LinkedListViewModel: ObservableObject {
+    @Published var nodes: [Node<Int>] = []
+    
+    @Published var linkedList = LinkedList<Int>()
+    
+    @Published var startValue: Int?
+    
+    @Published var sliceValues: [Int]?
+    
+    var anyCancellables: Set<AnyCancellable> = []
+    
+    init() {
+        $linkedList
+            .delay(for: .seconds(0.1), scheduler: RunLoop.main)
+            .sink { [weak self] _ in
+                self?.nodes = self?.linkedList.nodes ?? []
+            }
+            .store(in: &anyCancellables)
+    }
+    
+    func push() {
+        linkedList.push(3)
+        linkedList.push(2)
+        linkedList.push(1)
+    }
+    
+    func append() {
+        linkedList.append(4)
+    }
+    
+    func insert() {
+        let node = linkedList.node(at: 0)
+        guard let n = node else {
+            linkedList.push(100)
+            return
+        }
+        _ = linkedList.insert(100, after: n)
+    }
+    
+    func pop() {
+        _ = linkedList.pop()
+    }
+    
+    func removeLast() {
+        _ = linkedList.removeLast()
+    }
+    
+    func removeAfter() {
+        let node = linkedList.node(at: 1)
+        if node != nil {
+            _ = linkedList.remove(after: node!)
+        }
+    }
+    
+    func startIndex() {
+        self.startValue = linkedList[linkedList.startIndex]
+    }
+    
+    func prefix3() {
+        self.sliceValues = Array(linkedList.prefix(3))
+    }
+}
+
+struct LinkedListView: View {
+    @StateObject var viewModel = LinkedListViewModel()
+
+    var body: some View {
+        VStack(spacing: 20) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<self.viewModel.nodes.count, id: \.self) { index in
+                        NodeView(node: self.viewModel.nodes[index])
+                    }
+                }
+            }
+        
+            Button("push") {
+                viewModel.push()
+            }
+            Button("append") {
+                viewModel.append()
+            }
+            Button("insert") {
+                viewModel.insert()
+            }
+            Button("pop") {
+                viewModel.pop()
+            }
+            Button("removeLast") {
+                viewModel.removeLast()
+            }
+            Button("removeAfter") {
+                viewModel.removeAfter()
+            }
+            VStack {
+                Button("startIndex") {
+                    viewModel.startIndex()
+                }
+                Text("\(viewModel.startValue ?? 0)")
+            }
+            VStack {
+                Button("prefix3") {
+                    viewModel.prefix3()
+                }
+                Text("\(viewModel.sliceValues?.description ?? [].description)")
+            }
+
+        }
+        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct NodeView: View {
+    let node: Node<Int>
+    
+    var body: some View {
+        Text("\(node.value)")
+            .foregroundColor(.primary)
+            .padding()
+            .background(Color.green)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LinkArrow: View {
+    var body: some View {
+        HStack(spacing: 0) {
+            Color.primary
+                .frame(width: 20, height: 2)
+            Image(systemName: "arrowtriangle.right.fill")
+                .foregroundColor(.primary)
+                .imageScale(.small)
+                .scaledToFill()
+        }
+    }
+}
+
+struct LinkedListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LinkedListView()
+    }
+}
